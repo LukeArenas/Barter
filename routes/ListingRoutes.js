af3793136b4b4de08f24e5450d8ac31afe4984aa
Luke Arenas <lukedarenas@gmail.com>
2021-03-01 13:46:09 -0700

    added listing routes and tested

--- routes/ListingRoutes.js
+++ routes/ListingRoutes.js
@@ -2,5 +2,7 @@ const Router = require('express').Router()
 const controller = require('../controllers/ListingController')
 
 Router.get('/listings', controller.getAllListings)
+Router.post('/listings', controller.createListing)
+Router.delete('/listings/:id', controller.deleteListing)
 
 module.exports = Router
