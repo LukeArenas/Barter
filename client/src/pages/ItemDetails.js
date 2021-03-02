13c72be1ba226e5ae5ea3fdf030b07f72b3e69a0
Luke Arenas <lukedarenas@gmail.com>
2021-03-02 09:32:13 -0700

    created axios call and displayed result on ItemDetails page

--- client/src/pages/ItemDetails.js
+++ client/src/pages/ItemDetails.js
@@ -1,7 +1,57 @@
 import React, { Component } from 'react'
+import { BASE_URL } from '../globals'
+import axios from 'axios'
 
 export default class ItemDetails extends Component {
+  constructor() {
+    super()
+    this.state = {
+      category: '',
+      condition: '',
+      description: '',
+      photo: '',
+      price: 0,
+      title: ''
+    }
+  }
+  componentDidMount() {
+    this.displayListing()
+  }
+
+  displayListing = async () => {
+    const response = await axios.get(
+      `${BASE_URL}/listings/${this.props.selectedListing}`
+    )
+    const {
+      category,
+      condition,
+      description,
+      photo,
+      price,
+      title
+    } = response.data.listing
+
+    this.setState({
+      category: category,
+      condition: condition,
+      description: description,
+      photo: photo,
+      price: price,
+      title: title
+    })
+  }
   render() {
-    return <div></div>
+    return (
+      <div>
+        <img src={this.state.photo} alt={this.state.title} />
+        <div>
+          <h3>{this.state.title}</h3>
+          <p>{this.state.price}</p>
+          <p>{this.state.condition}</p>
+          <p>{this.state.description}</p>
+        </div>
+        <button>Add To Cart</button>
+      </div>
+    )
   }
 }
