f8b00da6aabfa0ff9226b9f720d63f861c120bf0
Luke Arenas <lukedarenas@gmail.com>
2021-03-01 17:30:27 -0700

    built out ListingThumbnail component

--- client/src/components/ListingThumbnail.js
+++ client/src/components/ListingThumbnail.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react'
 
 export default class ListingThumbnail extends Component {
   render() {
-    return <div>hello</div>
+    const { photo, price, title } = this.props.listing
+    return (
+      <div>
+        <img src={photo} alt={title} />
+        <h4>{title}</h4>
+        <p>{price}</p>
+      </div>
+    )
   }
 }
