99739359925f464a6d6ca054b346578773b193b7
Luke Arenas <lukedarenas@gmail.com>
2021-03-04 09:33:26 -0700

    fixed bug with delete button

--- client/src/components/ListingThumbnail.js
+++ client/src/components/ListingThumbnail.js
@@ -9,21 +9,29 @@ export default class ListingThumbnail extends Component {
   render() {
     const { photo, price, title, _id, seller_id } = this.props.listing
     return (
-      <div className="thumbnail" value={_id} onClick={this.handleClick}>
-        <div>
+      <div className="thumbnail">
+        <div
+          className="thumbnail-content"
+          value={_id}
+          onClick={this.handleClick}
+        >
           <img src={photo} alt={title} className="thumbnail-img" />
           <h4 className="thumbnail-title">{title}</h4>
           <p className="price">${price}</p>
         </div>
         {seller_id === this.props.currentSellerId &&
         this.props.requestFrom === 'sell' ? (
-          <button
-            value={_id}
-            onClick={this.props.deleteListing}
-            className="delete-button"
-          >
-            X
-          </button>
+          <div className="dropdown">
+            <div class="dropbtn">
+              <button
+                value={_id}
+                onClick={this.props.deleteListing}
+                className="delete-button"
+              >
+                X
+              </button>
+            </div>
+          </div>
         ) : null}
       </div>
     )
