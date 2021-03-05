f66a2bec2c9631a3037b53cd7303b24f4f411245
Luke Arenas <lukedarenas@gmail.com>
2021-03-05 14:46:13 -0700

    added specs to deploy to heroku

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ const sellerRoutes = require('./routes/SellerRoutes')
 const bodyParser = require('body-parser')
 const logger = require('morgan')
 const cors = require('cors')
+const path = require('path')
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3002
 
 const app = express()
 app.use(bodyParser.json())
@@ -17,5 +18,12 @@ app.use('/api', listingRoutes)
 app.use('/api', sellerRoutes)
 
 db.on('error', console.error.bind(console, 'MongoDB connection error: '))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(`${__dirname}/client/build/index.html`))
+  })
+}
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
